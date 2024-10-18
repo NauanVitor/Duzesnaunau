b2990d6a3e0c82b8eb1397030dfb33f27fef5516
NauanVitor <142450673+NauanVitor@users.noreply.github.com>
2024-10-18 08:22:10 -0300

    ,

---
+++ perguntas.js
@@ -0,0 +1,124 @@
+criaCartão(
+    'Historia',
+    'Quando acabou a segundo Guerra mundial?',
+    'Acabou em 1945'
+)
+
+criaCartão(
+    'Biologia',
+    'Qual o maior osso do corpo humano?',
+    'Femur'
+)
+
+criaCartão(
+    'Biologia',
+    'Qual o maior orgão do corpo humano?',
+    'A pele'
+)
+
+criaCartão(
+    'Geografia',
+    'Qual a capital da irlanda?',
+    'Dublin'
+)
+
+criaCartão(
+    'Geografia',
+    'Qual os três países mais ricos do mundo? ',
+    'Luxemburgo,Irlanda e Suiça'
+)
+
+criaCartão(
+    'Matematica',
+    'Qual o valor de pi?',
+    '3,14'
+) 
+
+criaCartão(
+    'Português',
+    'O que é anomatopéia?',
+    'Onomatopeia ou mimologia é uma figura de linguagem na qual se reproduz um som por meio de fonemas. A forma adjetiva é onomatopeico ou onomatopaico.'
+)
+
+criaCartão(
+    'Historia',
+    'Por que o muro de Berlim caiu?',
+    'A queda do Muro de Berlim foi consequência da crise enfrentada pelo bloco socialista na década de 1980.'
+)
+
+criaCartão(
+    'Geografia',
+    'Qual o menor pais do mundo?',
+    'Vaticano'
+)
+
+
+criaCartão(
+    'Historia/Biologia',
+    'quem foi charles darwin?',
+    'Charles Robert Darwin foi um naturalista, geólogo e biólogo britânico, célebre por seus avanços sobre evolução nas ciências biológicas.'
+)
+
+criaCartão(
+    'Sociologio',
+    'Como o acumulo de capital afeta a sociedade?',
+    'Criando desigualdade e concentração de capital em em um só polo da sociedade'
+)
+
+criaCartão(
+    'Filosofia',
+    'Qual foi a bro mais importante de Friedrich Nietzsche?',
+    'Assim falou zaratustra'
+)
+
+criaCartão(
+    'Matematica',
+    'Como medir a área de um quadrado?',
+    'Base*Altura= área do mesmo '
+)
+
+criaCartão(
+    'Quimica',
+    'quantos elementos tem na tabela periódica?',
+    '118 elementos quimicos'
+)
+
+criaCartão(
+    'fisica',
+    'Como calcular velocidado do objeto?',
+    'Deslocamento sobre a alteração no tempo (Δt) representado pela equação v = Δs/Δt.'
+)
+
+criaCartão(
+    'Artes',
+    'Qual as cores primarias?',
+    'Vermelho,Azul e Amarelo'
+)
+
+criaCartão(
+    'Artes',
+    'Qual foi o mais famoso pintor da historia?',
+    'Leonardo da vinci'
+)
+
+criaCartão(
+    'Historia',
+    'Quando foi abolida a escravidão no Brasil?',
+    'Foi em 1888'
+)
+
+criaCartão(
+    'Historia',
+    'Diga três leis Abolicionistas?',
+    'Lei Eusébio de queiros, Lei Aurea e Lei do ventre livre'
+)
+
+criaCartão(
+    'Matematica',
+    'Pergunta',
+    'Respostas'
+)
+
+
+
+
