11c0dd129bd8f02dc89554542bf53c9e86000fa0
NauanVitor <142450673+NauanVitor@users.noreply.github.com>
2024-10-25 12:24:22 -0300

    d

--- pagina2.js
+++ pagina2.js
@@ -19,7 +19,7 @@ function criaCartão (categoria, pergunta, resposta){
     function mostraResposta(card){
         const resposta = card.querySelector('.resposta-cartao');
         if(resposta.style.display === 'none'){
-            resposta.style.display === 'block'    
+            resposta.style.display = 'block'    
         }
         else{
         resposta.style.display === 'none'
